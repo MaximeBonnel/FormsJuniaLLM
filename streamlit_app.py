e6c9dcc4da7feda8209d81f19c3446daf426b705
MaximeBonnel <maxime.bonnel@student.junia.com>
2024-04-03 10:28:44 +0200

    END

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from pymongo import MongoClient
+from streamlit_js_eval import streamlit_js_eval
+import pandas as pd
 
 st.set_page_config(page_title='Junia LLM')
 
@@ -12,42 +14,35 @@ client = init_connection()
 db = client["JuniaLLM"]
 collection = db["Q&R"]
 
+# Get example data
+examples = collection.find({}, {'_id': 0})
+
 # Page title
 st.title('Junia LLM')
 st.markdown("---")
 st.info("Veuillez entrer une question et une réponse en rapport avec Junia, l'ISEN, l'école...\nVoir les exemples ci-dessous.")
 
-examples = [
-    ["Quand doit-on choisir une spécialité à l'ISEN ?", "Vous devez choisir une spécialité en fin de 3ème année."], 
-    ["Quel est le prix du RU ?", "Le prix du RU est de 3,30€ pour les non-boursiers et de 1€ pour les boursiers."]
-]
-
-# Write examples
-st.write("- **Question**: " + examples[0][0])
-st.write("- **Réponse**: " + examples[0][1])
-st.write("*Ou*")
-st.write("- **Question**: " + examples[1][0])
-st.write("- **Réponse**: " + examples[1][1])
-
-st.markdown("---")
-
 # Champ de saisie pour la question
-question = st.text_input("Question :", "")
+question = st.text_area("Question :", value='')
 
 # Champ de saisie pour la réponse
-reponse = st.text_input("Réponse :", "")
+response = st.text_area("Réponse :", value='')
 
 # Bouton pour valider
 if st.button("Ajouter"):
     if question.strip() == "":
         st.error("Veuillez saisir une question.")
-    elif reponse.strip() == "":
+    elif response.strip() == "":
         st.error("Veuillez saisir une réponse.")
     else:
         # Traitement et affichage de la question et de la réponse
-        doc = {"instruction": question, "output": reponse}
+        doc = {"instruction": question, "output": response}
         collection.insert_one(doc)
         st.success("Question et réponse envoyées avec succès !")
-        st.question = ""
-        st.reponse = ""
-        st.experimental_rerun()
+        streamlit_js_eval(js_expressions="parent.window.location.reload()")
+
+
+st.markdown("---")
+st.markdown("### Exemples")
+df = pd.DataFrame(list(examples))
+st.write(df)
